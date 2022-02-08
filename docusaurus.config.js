302d700ea08c40a7ca9bcf24cc32f5845007dc2e
lcf <objccf@gmail.com>
2022-02-08 18:05:14 +0800

    修改配置文件

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,16 +4,24 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  // 网站的标题。
+  title: '鲤鱼池',
+  // 网站的标语
+  tagline: '记录学习日常及杂七杂八的东西',
+  // 网站的URL。这也可以被视为顶级主机名
+  url: 'https://wiki.ioscarp.com',
+  // 您网站的基本URL
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  // 网站图标
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  // GitHub用户或组织拥有此源代码存储库。部署命令（部署命令）将使用此参数。
+  organizationName: 'carppond', // Usually your GitHub org/user name.
+  // GitHub源代码存储库的名称。
+  projectName: 'WiKi', // Usually your repo name.
 
+  /** 预设所包含的插件或主题指定参数*/
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -37,75 +45,111 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
     ],
   ],
 
+  // 主题配置
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+      hideableSidebar: false,
+      colorMode: {
+
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+    
+        // Dark/light switch icon options
+        switchConfig: {
+          // Icon for the switch while in dark mode
+          darkIcon: '🌙',
+          lightIcon: '🌞',
+    
+          // CSS to apply to dark icon,
+          // React inline style object
+          // see https://reactjs.org/docs/dom-elements.html#style
+          darkIconStyle: {
+            marginLeft: "2px",
+          },
+    
+          // Unicode icons such as '\u2600' will work
+          // Unicode with 5 chars require brackets: '\u{1F602}'
+          //lightIcon: '\u{1F602}',
+    
+          lightIconStyle: {
+            marginLeft: "1px",
+          },
         },
+      },
+      
+      navbar: {
+        title: "鲤鱼池",
+        hideOnScroll: false,
+
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
+            to: "blog",
+            label: "博客",
+            position: "right",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            // href: "http://digest.wiki-power.com/",
+            to: "blog",
+            label: "书籍",
+            position: "right",
+          },
+          {
+            href: 'https://github.com/carppond',
             label: 'GitHub',
             position: 'right',
           },
         ],
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
+      // footer: {
+      //   style: 'dark',
+      //   links: [
+      //     {
+      //       title: 'Docs',
+      //       items: [
+      //         {
+      //           label: 'Tutorial',
+      //           to: '/docs/intro',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'Community',
+      //       items: [
+      //         {
+      //           label: 'Stack Overflow',
+      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //         },
+      //         {
+      //           label: 'Discord',
+      //           href: 'https://discordapp.com/invite/docusaurus',
+      //         },
+      //         {
+      //           label: 'Twitter',
+      //           href: 'https://twitter.com/docusaurus',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'More',
+      //       items: [
+      //         {
+      //           label: 'Blog',
+      //           to: '/blog',
+      //         },
+      //         {
+      //           label: 'GitHub',
+      //           href: 'https://github.com/facebook/docusaurus',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
