b5d167914661d7da8b3c00005157c66ddab6e9e4
lcf <objccf@gmail.com>
2022-02-09 16:13:25 +0800

    重置项目

---
+++ docs/Home.md
@@ -0,0 +1,21 @@
+---
+id: Home
+title: Power's Wiki
+sidebar_label: Home
+slug: /
+---
+
+<!--  一个不会讲故事的攻城狮，算不上一个很酷的产品汪~-->
+
+Hi there~  
+这是我的知识库。
+
+- 鲤鱼 / 男
+- 职业：iOS开发工程师
+- 技能：移动端性能优化、逆向与安全
+
+[![GitHub deployments](https://img.shields.io/github/deployments/linyuxuanlin/Wiki_Docusaurus/Production?label=Build&style=flat-square)](https://vercel.com/linyuxuanlin/wiki-docusaurus/deployments)
+[![GitHub last commit](https://img.shields.io/github/last-commit/linyuxuanlin/Wiki_Docusaurus?color=FCD734&label=Last%20commit&style=flat-square)](https://github.com/linyuxuanlin/Wiki_Docusaurus/commits/main)
+[![contact](https://img.shields.io/badge/Contact%20me-here-34ABE0?&style=flat-square)](ContactMe)
+
+<!-- <h6>Power Lin |  <a href="https://beian.miit.gov.cn"> 粤 ICP 备 20014898 号 </a> | Built with Docusaurus</h6> -->
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,7 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+// const lightCodeTheme = require('prism-react-renderer/themes/github');
+// const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
@@ -15,41 +17,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   // 网站图标
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.jpg',
   // GitHub用户或组织拥有此源代码存储库。部署命令（部署命令）将使用此参数。
   organizationName: 'carppond', // Usually your GitHub org/user name.
   // GitHub源代码存储库的名称。
   projectName: 'WiKi', // Usually your repo name.
 
-  /** 预设所包含的插件或主题指定参数*/
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-
   // 主题配置
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      hideableSidebar: false,
+      hideableSidebar: false, // 让边栏可收起展开
       colorMode: {
 
         disableSwitch: false,
@@ -79,8 +57,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
       },
       
       navbar: {
-        title: "鲤鱼池",
-        hideOnScroll: false,
+        title: "鲤鱼池", //导航上的站点名称
+        hideOnScroll: false, // 文档页面向下滚动时收起顶部导航
 
         // logo: {
         //   alt: 'My Site Logo',
@@ -88,9 +66,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         // },
         items: [
           {
-            to: "blog",
-            label: "博客",
-            position: "right",
+            href: 'https://ioscarp.com', // 点击后跳转的链接,站内跳转用 to,占位用 href
+            label: "博客", // 显示的名称
+            position: "right", // 显示在导航的左边还是右边
           },
           {
             // href: "http://digest.wiki-power.com/",
@@ -150,9 +128,63 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
       //   ],
       //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       // },
+      // prism: {
+      //   theme: lightCodeTheme,
+      //   darkTheme: darkCodeTheme,
+      // },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: require('prism-react-renderer/themes/dracula'),
       },
     }),
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
+      integrity:
+        'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
+      crossorigin: 'anonymous',
+    },
+  ],
+  /** 预设所包含的插件或主题指定参数*/
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarCollapsible: true, //默认折叠
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"), // 侧边栏
+          showLastUpdateAuthor: false,
+          showLastUpdateTime: false,
+          editUrl: "https://github.com/linyuxuanlin/Wiki_Docusaurus/edit/main/",
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        },
+        blog: {
+          //blogTitle: 'Power\'s blog!',
+          //blogDescription: 'A docusaurus powered blog!',
+          blogSidebarCount: 8,
+          postsPerPage: 8,
+          showReadingTime: false,
+          path: 'blog',
+          blogSidebarTitle: 'Recent',
+          editUrl: 'https://github.com/linyuxuanlin/Wiki_Docusaurus/edit/main/',
+          /*
+          feedOptions: {
+            type: 'all', // required. 'rss' | 'feed' | 'all'
+            title: 'Power\'s Blog', // default to siteConfig.title
+            description: '个人博客', // default to  `${siteConfig.title} Blog`
+            copyright: 'Copyright © ${new Date().getFullYear()} Power Lin',
+            language: undefined, // possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+          },
+          */
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+  
 });
