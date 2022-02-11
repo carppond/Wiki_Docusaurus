43eb609006b54c4448322583f454e9a4467a5753
lcf <objccf@gmail.com>
2022-02-11 10:18:35 +0800

    更改链接

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -157,7 +157,7 @@ const katex = require('rehype-katex');
           sidebarPath: require.resolve("./sidebars.js"), // 侧边栏
           showLastUpdateAuthor: false,
           showLastUpdateTime: false,
-          editUrl: "https://github.com/linyuxuanlin/Wiki_Docusaurus/edit/main/",
+          editUrl: "https://github.com/carppond/Wiki_Docusaurus/tree/edit/docs",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
@@ -169,7 +169,7 @@ const katex = require('rehype-katex');
           showReadingTime: false,
           path: 'blog',
           blogSidebarTitle: 'Recent',
-          editUrl: 'https://github.com/linyuxuanlin/Wiki_Docusaurus/edit/main/',
+          editUrl: 'https://github.com/carppond/Wiki_Docusaurus/tree/edit/blog',
           /*
           feedOptions: {
             type: 'all', // required. 'rss' | 'feed' | 'all'
